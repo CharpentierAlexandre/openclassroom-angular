bb59676ddf1bfe2d03f03c4c957539b84ee6041b
Alexandre Charpentier <alexandre.charpentier@epnak.org>
2019-02-01 11:04:07 +0100

    clean project ex1 angular OCR

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DeviceComponent } from './device/device.component';
 import { FormsModule } from '@angular/forms';
 import { PostListComponent } from './post-list/post-list.component';
 import { PostListItemComponent } from './post-list-item/post-list-item.component';
@@ -11,7 +10,6 @@ import { PostListItemComponent } from './post-list-item/post-list-item.component
 @NgModule({
   declarations: [
     AppComponent,
-    DeviceComponent,
     PostListComponent,
     PostListItemComponent
   ],
